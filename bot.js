57c6314fcf2b5b27c0ba42f833e86fcf513c9fbc
AleksanderV <vypovaleksandr@gmail.com>
2021-10-15 11:36:28 +0300

    all done

---
+++ bot.js
@@ -0,0 +1,35 @@
+const TelegramBot = require('node-telegram-bot-api');
+const axios = require('axios');
+require('dotenv').config()
+
+const bot = new TelegramBot(process.env.TG_TOKEN, { polling: true });
+
+bot.on('voice', (msg) => {
+  const stream = bot.getFileStream(msg.voice.file_id);
+
+  let chunks = [];
+  
+
+  stream.on('data', (chunk) => chunks.push(chunk));
+  stream.on('end', async () => {
+    const axiosConfig = {
+      method: 'POST',
+      url: 'https://stt.api.cloud.yandex.net/speech/v1/stt:recognize',
+      headers: {
+        Authorization: `Api-Key ${process.env.API_YA_KEY}`,
+      },
+      data: Buffer.concat(chunks),
+    };
+    try {
+      const response = await axios(axiosConfig)
+      const {result} = response.data;
+      const chatId = msg.chat.id;
+      const userName = msg.from.first_name
+      console.log(result)
+      bot.sendMessage(chatId, `${userName} говорит:\n${result}`)
+    } catch (error) {
+      console.log('Error:', error);
+    }
+    });
+  });
+
